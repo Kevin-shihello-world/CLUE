78e585bac4529cdf121240d3f71d8bab7a436c00
Kevin-shihello-world <32519859+Kevin-shihello-world@users.noreply.github.com>
2021-09-09 10:30:31 +0800

    Update pytorch_modeling.py

--- baselines/models_pytorch/mrc_pytorch/pytorch_modeling.py
+++ baselines/models_pytorch/mrc_pytorch/pytorch_modeling.py
@@ -31,7 +31,7 @@ from torch.nn import CrossEntropyLoss
 from torch.nn import functional as F
 from torch.distributions.bernoulli import Bernoulli
 
-from mnt.CLUE_master.baselines.models_pytorch.mrc_pytorch.tools.file_utils import cached_path
+from ..tools.file_utils import cached_path
 
 logger = logging.getLogger(__name__)
 
